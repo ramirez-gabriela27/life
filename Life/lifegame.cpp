8db9d7795505c0d6907d9c142c1c17d1ce126499
Gubby <tolosaramirez.gabriela12@hotmail.com>
2021-04-03 17:29:59 -0600

    added temps in cpp

--- Life/lifegame.cpp
+++ Life/lifegame.cpp
@@ -3,3 +3,36 @@
 //location struct
 //cell class finished in header
 //game class needs completion
+
+//member functions
+void Game::take_turn(){
+    return;
+}
+
+void Game::start_game(){
+    return;
+}
+
+void Game::create_grid(){
+    return;
+}
+
+void Game::create_graph(){
+    return;
+}
+
+void Game::start_simulation(){
+    return;
+}
+
+void Game::update_grid(){
+    return;
+}
+
+void Game::update_graph(){
+    return;
+}
+
+void Game::pause_simulation(){
+    return;
+}
